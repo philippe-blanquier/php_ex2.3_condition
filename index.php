c4f3d2b427b14887198dd62b6391e20eb9bacb47
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 14:41:56 +0200

    evo

--- index.php
+++ index.php
@@ -1,38 +1,42 @@
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
-        <title>PHP ex2.3 condition</title>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>PHP ex 2.3 condition</title>
     </head>
     <body >
-        <?php
-        $age=21;
-        $gender="Homme"; // Femme
-        if ($age >= 18)
-        {
-            if ($gender == "Homme")
+        <p>
+            <?php
+            $age=21;
+            $gender="Homme"; // Femme
+            if ($age >= 18)
             {
-                $maj_min="majeur";
-                $type="un homme";
+                if ($gender == "Homme")
+                {
+                    $maj_min="majeur";
+                    $type="un homme";
+                }
+                else
+                {
+                    $maj_min="majeure";
+                    $type="une femme";
+                }
             }
             else
             {
-                $maj_min="majeure";
-                $type="une femme";
+                $maj_min="mineur";
+                if ($gender == "Homme")
+                {
+                    $type="un homme";
+                }
+                else
+                {
+                    $type="une femme";
+                }
             }
-        }
-        else
-        {
-            $maj_min="mineur";
-            if ($gender == "Homme")
-            {
-                $type="un homme";
-            }
-            else
-            {
-                $type="une femme";
-            }
-        }
-        echo ("Vous êtes ".$type." et vous êtes ".$maj_min)
-       ?>
+            echo ("Vous êtes ".$type." et vous êtes ".$maj_min)
+            ?>
+        </p>
     </body>
 </html>
